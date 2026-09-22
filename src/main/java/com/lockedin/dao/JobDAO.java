2eb4bfb8fd10fd90a10e3919ac0c2c6a3d0b369d
Jumana Ayoub <ayoub.jumana1@gmail.com>
2026-09-21 22:46:08 -0700

    Update JobDAO.java

--- src/main/java/com/lockedin/dao/JobDAO.java
+++ src/main/java/com/lockedin/dao/JobDAO.java
@@ -2,8 +2,16 @@ package com.lockedin.dao;
 
 /**
  * Provides data-access operations for job postings.
- * 
+ *
  * @author Jumana
  */
 public class JobDAO {
+
+    /**
+     * Temporarily returns a hardcoded number of open jobs
+     * while servlet/JSP integration is being tested.
+     */
+    public int getOpenJobCount() {
+        return 10;
+    }
 }
